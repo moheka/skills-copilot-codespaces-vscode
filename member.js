90e96a2489c3bc88b92c6ca6287b441e23fe2ac4
moheka <129864989+moheka@users.noreply.github.com>
2023-07-28 18:45:19 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,18 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    member.style.display = "block";
+    var skills = document.getElementById("skills");
+    skills.style.display = "none";
+    var projects = document.getElementById("projects");
+    projects.style.display = "none";
+    var contact = document.getElementById("contact");
+    contact.style.display = "none";
+    var about = document.getElementById("about");
+    about.style.display = "none";
+    var home = document.getElementById("home");
+    home.style.display = "none";
+    var login = document.getElementById("login");
+    login.style.display = "none";
+    var register = document.getElementById("register");
+    register.style.display = "none";
+}
